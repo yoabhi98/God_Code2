a06ceff116a00d035780f63399027a58ca2149a4
Abhishek Mishra <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 22:34:20 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Armstrong
+{
+
+     
+public static void main(String []args)
+{
+  
+int n=0,rem,result=0;           
+Scanner sc=new Scanner(System.in);
+
+int num = sc.nextInt();
+int temp=num;
+while(num>0)
+{
+num/=10;
+n++;
+}
+num=temp;
+while(num>0)
+{
+rem=num%10;
+result+=Math.pow(rem,n);
+num/=10;
+}
+if(result==temp)
+System.out.println("yes");
+else
+System.out.println("no");
+}
+}
