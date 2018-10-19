e677c432f021f0ba40ef9a46841fa5e3f04d8e94
Abhishek Mishra <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 17:08:57 +0530

    Add files via upload

---
+++ Palindrone.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Palindrone
+{
+
+     
+public static void main(String []args)
+{
+               
+Scanner sc=new Scanner(System.in);
+
+int rem,result=0;
+int num = sc.nextInt();
+int x=num;
+while(num>0)
+{
+rem=num%10;
+result=result*10+rem;
+num/=10;
+}
+System.out.println(result);
+if(result==x) 
+System.out.println("yes");
+else
+System.out.println("no");    
+}
+}
