b7beca1f7f588fb83adbae43cd7d02c173d63b2e
Abhishek Mishra <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 16:48:26 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Power
+{
+
+     
+public static void main(String []args)
+{
+               
+Scanner sc=new Scanner(System.in);
+
+int result=1;
+int a = sc.nextInt();
+int b = sc.nextInt();
+while(b>0)
+{
+result*=a;
+b--;
+} 
+System.out.println(result);     
+}
+}
