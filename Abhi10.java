5f573a304965de3edbc0ca8a992d8484ea6c3ac0
Abhishek Mishra <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 23:28:30 +0530

    Update Abhi10.java

--- Abhi10.java
+++ Abhi10.java
@@ -9,8 +9,7 @@ public static void main(String []args)
 int fact=5;            
 Scanner sc=new Scanner(System.in);
 
-int num = sc.nextInt();
-num++;    
+int num = sc.nextInt();    
 while(fact>0)
 {
 if(num%5==0)
