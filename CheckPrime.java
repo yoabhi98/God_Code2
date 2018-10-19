0a3b261e4d786ce4f45fd5bcc54b7e714957b36f
Abhishek Mishra <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 22:04:11 +0530

    Add files via upload

---
+++ CheckPrime.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class CheckPrime
+{
+
+     
+public static void main(String []args)
+{
+             
+Scanner sc=new Scanner(System.in);
+
+int low = sc.nextInt();
+int high = sc.nextInt();
+low++;
+    while (low < high) {
+            boolean flag = false;
+
+            for(int i = 2; i <= low/2; ++i) {
+                
+                if(low % i == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag)
+                System.out.print(low + " ");
+
+            ++low;
+        }
+
+}
+}
