0c8495c7e0ea69b5a42dbc22320381d8d8791263
Abhishek Mishra <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 23:26:48 +0530

    Add files via upload

---
+++ Abhi10.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Abhi10
+{
+
+     
+public static void main(String []args)
+{
+ 
+int fact=5;            
+Scanner sc=new Scanner(System.in);
+
+int num = sc.nextInt();
+num++;    
+while(fact>0)
+{
+if(num%5==0)
+{
+System.out.print(num+" ");
+fact--;
+}
+num++;
+}
+}
+}
