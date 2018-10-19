011895da73662e60db6b096bc49a36213763faae
Abhishek Mishra <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 17:33:44 +0530

    Add files via upload

---
+++ Prime2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Prime2
+{
+
+     
+public static void main(String []args)
+{
+               
+Scanner sc=new Scanner(System.in);
+
+int result=1;
+int a = sc.nextInt();
+int b = sc.nextInt();
+for(++a;a<b;a++)
+{
+if(a%2==0)
+System.out.println(a);
+}
+System.out.println(" ");     
+}
+}
