316bba4b13575698c287c5b7028219cb926e5061
Abhishek Mishra <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 17:23:52 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Prime
+{
+
+     
+public static void main(String []args)
+{
+  
+int i;             
+Scanner sc=new Scanner(System.in);
+
+int num = sc.nextInt();
+for(i=2;i<=num/2;i++)
+{
+if(num%i==0)
+break;
+}
+if(i==num) 
+System.out.println("prime");  
+else
+System.out.println("prime");    
+}
+}
